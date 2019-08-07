dc0f6d522ef27a94f7b4ee9f561e55acaaf9caaf
Zafar Kudratov <zafarkudratov@gmail.com>
2019-08-07 07:45:57 +0300

    bla-bla-bla

--- test/counterTest.js
+++ test/counterTest.js
@@ -1,10 +1,9 @@
 const assert = require('chai').assert;
 const couter = require('../counter');
 //Results
+counterResult = counter.counter();
 
-
-describe('Counter', () => {
-    counterResult = counter.counter();
+describe('task testing counter', () => {
     describe('counter()', () => {
         it('counter should return typof number' () => {
             assert.typeof(counterResult, 'number')
